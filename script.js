17bb3625f709e8d468567690ea6826b1f46edeab
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-05-13 13:51:26 +0200

    Added "playRound()"
    Added the function "playRound()", which compares the choices by computer and
    player to determine who wins the round.

--- script.js
+++ script.js
@@ -16,10 +16,26 @@ function computerPlay() {
   return computerSelection;
 }
 
-function playRound(playerSelection, computerSelection) {}
+function playRound(playerSelection, computerSelection) {
+  if (computerSelection === playerSelection) {
+    return "It's a tie!";
+  } else if (computerSelection === "rock" && playerSelection === "scissors") {
+    return "The Computer wins!";
+  } else if (computerSelection === "paper" && playerSelection === "rock") {
+    return "The Computer wins!";
+  } else if (computerSelection === "scissors" && playerSelection === "paper") {
+    return "The Computer wins!";
+  } else {
+    return "The Player wins!";
+  }
+}
 
 computerSelection = computerPlay();
-playerSelection = prompt().toLowerCase();
+playerSelection = prompt(
+  "Please enter 'Rock', 'Paper or 'Scissors'."
+).toLowerCase();
 
 console.log(`Computer chooses ${computerSelection}`);
 console.log(`Player chooses ${playerSelection}`);
+
+console.log(playRound(playerSelection, computerSelection));
