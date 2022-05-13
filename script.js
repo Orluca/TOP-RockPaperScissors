3ed4eb7fdd7f0403c7c830795bfe962c2f749b75
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-05-13 13:11:05 +0200

    Added function "computerPlay()"
    Added function "computerPlay()), which creates a random choice between "Rock", "Paper" or "Scissors".

---
+++ script.js
@@ -0,0 +1,21 @@
+let computerSelection;
+let playerSelection;
+
+function computerPlay() {
+  // Generate a random integer between 1 and 3
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+  // Depending on the generated number, set computerSelection to rock, paper or scissor
+  if (randomNumber === 1) {
+    computerSelection = "rock";
+  } else if (randomNumber === 2) {
+    computerSelection = "paper";
+  } else {
+    computerSelection = "scissors";
+  }
+  return computerSelection;
+}
+
+computerSelection = computerPlay();
+
+console.log(`Computer chooses ${computerSelection}`);
