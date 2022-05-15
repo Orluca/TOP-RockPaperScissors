4bba0c55b7cdf4931e200fa41e022c3462321546
lucor23 <67704819+lucor23@users.noreply.github.com>
2022-05-15 21:24:10 +0200

    A bit of code cleanup

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ let playerSelection;
 let computerScore = 0;
 let playerScore = 0;
 
+// Variables for HTML objects
+const ElWeaponIconComputer = document.getElementById("computer-weapon");
+const ElWeaponIconPlayer = document.getElementById("player-weapon");
+const ElScoreComputer = document.getElementById("computer-score");
+const ElScorePlayer = document.getElementById("player-score");
+const ElFinalScorePlayer = document.getElementById("player-score-final");
+const ElFinalScoreComputer = document.getElementById("computer-score-final");
+const ElPopupWindow = document.getElementById("popup-window-background");
+const ElPopupWindowText = document.getElementById("popup-text");
+const ElPopupOverlay = document.getElementById("popup-overlay");
+const ElWinnerSign = document.getElementById("winner-sign");
+
 function computerPlay() {
   // THIS FUNCTION RANDOMLY DETERMINES A CHOICE BETWEEN "ROCK", "PAPER" OR "SCISSOR" FOR THE COMPUTER
 
@@ -13,13 +25,13 @@ function computerPlay() {
   // Depending on the generated number, set computerSelection to rock, paper or scissor. Also update the icon in the HTML accordingly.
   if (randomNumber === 1) {
     computerSelection = "rock";
-    document.getElementById("computer-weapon").innerHTML = "🗻";
+    ElWeaponIconComputer.innerHTML = "🗻";
   } else if (randomNumber === 2) {
     computerSelection = "paper";
-    document.getElementById("computer-weapon").innerHTML = "📃";
+    ElWeaponIconComputer.innerHTML = "📃";
   } else {
     computerSelection = "scissors";
-    document.getElementById("computer-weapon").innerHTML = "✂";
+    ElWeaponIconComputer.innerHTML = "✂";
   }
   return computerSelection;
 }
@@ -42,27 +54,25 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game(userChoice) {
-  const sign = document.getElementById("winner-sign");
-
   // First, determine the choices of both players
   computerSelection = computerPlay();
   playerSelection = userChoice;
 
   // Next, play a round with those choices
   if (playRound(playerSelection, computerSelection) === "IT'S A TIE!") {
-    sign.textContent = "It's a tie!";
-    sign.style.color = "antiquewhite";
+    ElWinnerSign.textContent = "It's a tie!";
+    ElWinnerSign.style.color = "antiquewhite";
   } else if (playRound(playerSelection, computerSelection) === "THE COMPUTER WINS!") {
-    sign.textContent = "THE COMPUTER WINS!";
-    sign.style.color = "rgb(255, 135, 135)";
+    ElWinnerSign.textContent = "THE COMPUTER WINS!";
+    ElWinnerSign.style.color = "rgb(255, 135, 135)";
     computerScore += 1;
-    document.getElementById("computer-score").textContent = computerScore;
+    ElScoreComputer.textContent = computerScore;
     playAnimation("computer-score", "wiggle-animation", 250);
   } else {
-    sign.textContent = "YOU WIN!";
-    sign.style.color = "rgb(159, 255, 85)";
+    ElWinnerSign.textContent = "YOU WIN!";
+    ElWinnerSign.style.color = "rgb(159, 255, 85)";
     playerScore += 1;
-    document.getElementById("player-score").textContent = playerScore;
+    ElScorePlayer.textContent = playerScore;
     playAnimation("player-score", "wiggle-animation", 250);
   }
   playAnimation("player-weapon", "grow-animation", 200);
@@ -74,18 +84,18 @@ function game(userChoice) {
 function checkTotalScore() {
   // THIS FUNCTION CHECKS THE CURRENT TOTAL SCORES AND ENDS THE GAME IF SOMEONE REACHES 5 POINTS
   if (computerScore === 5) {
-    document.getElementById("player-score-final").textContent = playerScore;
-    document.getElementById("computer-score-final").textContent = computerScore;
-    document.getElementById("popup-window-background").style.backgroundColor = "rgb(255, 179, 179)";
-    document.getElementById("popup-text").textContent = "THE COMPUTER WON!";
-    document.getElementById("popup-window").style.display = "block";
+    ElFinalScorePlayer.textContent = playerScore;
+    ElFinalScoreComputer.textContent = computerScore;
+    ElPopupWindow.style.backgroundColor = "rgb(255, 179, 179)";
+    ElPopupWindowText.textContent = "THE COMPUTER WON!";
+    ElPopupOverlay.style.display = "block";
     playAnimation("popup-window-background", "modal-window-animation", 1000);
   } else if (playerScore === 5) {
-    document.getElementById("player-score-final").textContent = playerScore;
-    document.getElementById("computer-score-final").textContent = computerScore;
-    document.getElementById("popup-window-background").style.backgroundColor = "rgb(213, 255, 161)";
-    document.getElementById("popup-text").textContent = "YOU WON!";
-    document.getElementById("popup-window").style.display = "block";
+    ElFinalScorePlayer.textContent = playerScore;
+    ElFinalScoreComputer.textContent = computerScore;
+    ElPopupWindow.style.backgroundColor = "rgb(213, 255, 161)";
+    ElPopupWindowText.textContent = "YOU WON!";
+    ElPopupOverlay.style.display = "block";
     playAnimation("popup-window-background", "modal-window-animation", 1000);
   }
 }
@@ -107,40 +117,33 @@ function endAnimation(id, name) {
 
 // LISTEN FOR BUTTON PRESSES
 document.getElementById("btn-rock").onclick = function () {
-  document.getElementById("player-weapon").textContent = "🗻";
+  ElWeaponIconPlayer.textContent = "🗻";
   game("rock");
 };
 
 document.getElementById("btn-paper").onclick = function () {
-  document.getElementById("player-weapon").textContent = "📃";
+  ElWeaponIconPlayer.textContent = "📃";
   game("paper");
 };
 
 document.getElementById("btn-scissors").onclick = function () {
-  document.getElementById("player-weapon").textContent = "✂";
+  ElWeaponIconPlayer.textContent = "✂";
   game("scissors");
 };
 
 // RESTART GAME BUTTON
 document.getElementById("btn-restart").onclick = function () {
   // Close the popup window
-  document.getElementById("popup-window").style.display = "none";
+  ElPopupOverlay.style.display = "none";
 
   // Reset the weapon symbols and the winner sign
-  document.getElementById("player-weapon").textContent = "";
-  document.getElementById("computer-weapon").textContent = "";
-  document.getElementById("winner-sign").textContent = "";
+  ElWeaponIconPlayer.textContent = "";
+  ElWeaponIconComputer.textContent = "";
+  ElWinnerSign.textContent = "";
 
   // Reset the score and update the scoreboard accordingly
   computerScore = 0;
   playerScore = 0;
-  document.getElementById("player-score").textContent = playerScore;
-  document.getElementById("computer-score").textContent = computerScore;
-};
-
-/*-------------------- TESTING AREA --------------------*/
-const testButton = document.getElementById("test-button");
-
-testButton.onclick = function () {
-  playAnimation("player-weapon", "grow-animation");
+  ElScorePlayer.textContent = playerScore;
+  ElScoreComputer.textContent = computerScore;
 };
